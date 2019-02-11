c60f000cacff5907c46dabf281bff8fb51a84236
Maniack Crudelis <maniackc_dev@crudelis.fr>
2019-02-11 20:21:00 +0100

    Normalization from example_ynh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,57 +1,29 @@
 #!/bin/bash
 
-# Copy files to the right place
-ampache_ynh_getsources () {
-	final_path=/var/www/$app
-	mkdir -p $final_path/log
-
-	ynh_setup_source $final_path
-
-	cp ../conf/ampache.cfg.php $final_path/config/ampache.cfg.php
-}
-
-# Change variables in Ampache configuration
-ampache_ynh_prepareconfig () {
-	ynh_replace_string "yunouser" "$app" $final_path/config/ampache.cfg.php
-	ynh_replace_string "yunopass" "$db_pwd" $final_path/config/ampache.cfg.php
-	ynh_replace_string "yunobase" "$app" $final_path/config/ampache.cfg.php
-	ynh_replace_string "PATHTOCHANGE" "$path_url" $final_path/config/ampache.cfg.php
-	ynh_replace_string "DOMAINTOCHANGE" "$domain" $final_path/config/ampache.cfg.php
-}
-
-# Ampache installation
-ampache_ynh_install () {
-        (
-		export COMPOSER_HOME=$final_path
-		cd $final_path
-		php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-		php composer-setup.php
-		#php composer.phar update --no-interaction
-		php composer.phar install --prefer-source --no-interaction --no-dev
-	)
-        # Set permissions to ampache directory
-        chown -R www-data: $final_path
-
-}
-
-# Regenerate SSOwat conf
-ampache_ynh_reloadservices () {
-	ynh_app_setting_set $app skipped_uris "/"
-	yunohost app ssowatconf
-}
-
-# Pre config ampache
-ampache_ynh_doconfig () {
-	sleep 1
-	ynh_local_curl /update.php?action=update
-	sleep 5
-}
+#=================================================
+# FUTURE OFFICIAL HELPERS
+#=================================================
 
 # Install or update the main directory yunohost.multimedia
 #
 # usage: ynh_multimedia_build_main_dir
 ynh_multimedia_build_main_dir () {
-	wget -nv https://github.com/YunoHost-Apps/yunohost.multimedia/archive/master.zip 2>&1
-	unzip -q master.zip
+	local ynh_media_release="v1.2"
+	local checksum="806a827ba1902d6911095602a9221181"
+
+	# Download yunohost.multimedia scripts
+	wget -nv https://github.com/YunoHost-Apps/yunohost.multimedia/archive/${ynh_media_release}.tar.gz 
+
+	# Check the control sum
+	echo "${checksum} ${ynh_media_release}.tar.gz" | md5sum -c --status \
+		|| ynh_die "Corrupt source"
+
+	# Check if the package acl is installed. Or install it.
+	ynh_package_is_installed 'acl' \
+		|| ynh_package_install acl
+
+	# Extract
+	mkdir yunohost.multimedia-master
+	tar -xf ${ynh_media_release}.tar.gz -C yunohost.multimedia-master --strip-components 1
 	./yunohost.multimedia-master/script/ynh_media_build.sh
 }
