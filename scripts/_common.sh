f137f8213873699a4b53e47dbe378ee17128d476
yalh76 <yalh@yahoo.com>
2020-05-26 15:58:48 +0200

    fix upgrade
    implmenting new helper from 3.8

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,8 +7,9 @@
 # dependencies used by the app
 pkg_dependencies="libav-tools|ffmpeg"
 
-extra_pkg_dependencies="php7.3-mysql php7.3-curl php7.3-simplexml php7.3-gd"
-# php7.3-gmp php7.3-dom php7.3-pcre php7.3-spl php7.3-simplexml php7.3-ctype php7.3-date php7.3-iconv php7.3-curl php7.3-gd  php7.3-mbstring php7.3-libxml php7.3-xmlwriter php7.3-xmlreader
+YNH_PHP_VERSION="7.3"
+
+extra_php_dependencies="php${YNH_PHP_VERSION}mysql php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-simplexml php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-ldap"
 
 #=================================================
 # EXPERIMENTAL HELPERS
@@ -16,8 +17,7 @@ extra_pkg_dependencies="php7.3-mysql php7.3-curl php7.3-simplexml php7.3-gd"
 
 # Execute a command with Composer
 #
-# usage: ynh_composer_exec [--phpversion=phpversion] [--workdir=$final_path] --commands="commands"
-# | arg: -v, --phpversion - PHP version to use with composer
+# usage: ynh_composer_exec --phpversion=phpversion [--workdir=$final_path] --commands="commands"
 # | arg: -w, --workdir - The directory from where the command will be executed. Default $final_path.
 # | arg: -c, --commands - Commands to execute.
 ynh_composer_exec () {
@@ -39,13 +39,12 @@ ynh_composer_exec () {
 
 # Install and initialize Composer in the given directory
 #
-# usage: ynh_install_composer [--phpversion=phpversion] [--workdir=$final_path] [--install_args="--optimize-autoloader"]
-# | arg: -v, --phpversion - PHP version to use with composer
+# usage: ynh_install_composer --phpversion=phpversion [--workdir=$final_path] [--install_args="--optimize-autoloader"]
 # | arg: -w, --workdir - The directory from where the command will be executed. Default $final_path.
 # | arg: -a, --install_args - Additional arguments provided to the composer install. Argument --no-dev already include
 ynh_install_composer () {
 	# Declare an array to define the options of this helper.
-	local legacy_args=vwa
+	local legacy_args=vw
 	declare -Ar args_array=( [v]=phpversion= [w]=workdir= [a]=install_args=)
 	local phpversion
 	local workdir
@@ -54,7 +53,6 @@ ynh_install_composer () {
 	ynh_handle_getopts_args "$@"
 	workdir="${workdir:-$final_path}"
 	phpversion="${phpversion:-7.0}"
-	install_args="${install_args:-}"
 
 	curl -sS https://getcomposer.org/installer \
 		| COMPOSER_HOME="$workdir/.composer" \
