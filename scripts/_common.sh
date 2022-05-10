9589628817a18607d5a23b286de3d3fe4a607ae6
ericgaspar <junk.eg@free.fr>
2022-05-10 13:51:27 +0200

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,10 +4,10 @@
 # COMMON VARIABLES
 #=================================================
 
-YNH_PHP_VERSION="7.4"
+YNH_PHP_VERSION="8.0"
 
 # Composer version
-YNH_COMPOSER_VERSION="2.3.3"
+YNH_COMPOSER_VERSION="2.3.5"
 
 pkg_dependencies="libav-tools|ffmpeg php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-simplexml php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-ldap"
 
