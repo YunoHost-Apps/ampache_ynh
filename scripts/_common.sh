4cec581f48bd405011581721f6dfca72b37ee90b
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-06-11 21:57:46 +0200

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,15 +4,8 @@
 # COMMON VARIABLES
 #=================================================
 
-#REMOVEME? YNH_PHP_VERSION=8.0
-
 # Composer version
 YNH_COMPOSER_VERSION="2.3.5"
-
-#REMOVEME? php_dependencies="php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-simplexml php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-ldap php${YNH_PHP_VERSION}-intl"
-
-# dependencies used by the app (must be on a single line)
-#REMOVEME? pkg_dependencies="libav-tools|ffmpeg $php_dependencies"
 
 #=================================================
 # PERSONAL HELPERS
